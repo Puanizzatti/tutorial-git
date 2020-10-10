aa7a62b905ac5940c14028d086cb11d0ca4a84fb
Puanizzatti <puanizzati13@gmail.com>
2020-10-10 13:44:01 -0700

    firs commit

---
+++ ContohBoolean.java
@@ -0,0 +1,13 @@
+public class ContohBoolean
+{
+	public static void main(String arg[])
+	{
+	boolean lapar;
+	lapar = true;
+	
+	while(lapar){
+		System.out.println("Kasih Makan dong...");
+		lapar = false;
+		}
+	}
+}
